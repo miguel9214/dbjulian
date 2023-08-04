1334445bcc83b3903cb54ea83317148db1e1baf8
mguel9214 <iguel921433@gmail.com>
2023-08-04 14:23:03 -0500

    terminado

--- js/app.js
+++ js/app.js
@@ -1,22 +1,38 @@
 const formulario = document.getElementById("formulario");
+const registro = document.getElementById("registro");
+const exito = document.getElementById("exito");
 
-formulario.addEventListener("submit", (e) => {
+formulario.addEventListener("submit", async (e) => {
   e.preventDefault();
+  try {
+    const respuesta = await fetch(
+      "https://sheet.best/api/sheets/ffc9e8ec-a978-40c7-a32a-d48712c90c44",
+      {
+        method: "POST",
+        mode: "cors",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          identificacionVotante: formulario.identVotante.value,
+          nombreCompleto: formulario.nombre.value,
+          celular: formulario.telefono.value,
+          barrio: formulario.barrio.value,
+          direccion: formulario.direccion.value,
+          identificacionLider: formulario.identLider.value,
+          nombreLider: formulario.identVotante.value,
+        }),
+      }
+    );
+  } catch (err) {
+    console.log(err);
+  }
 
-  fetch("https://sheet.best/api/sheets/ffc9e8ec-a978-40c7-a32a-d48712c90c44", {
-    method: "POST",
-    mode: "cors",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify({
-      identificacionVotante: formulario.identVotante.value,
-      nombreCompleto: formulario.nombre.value,
-      celular: formulario.telefono.value,
-      barrio: formulario.barrio.value,
-      direccion: formulario.direccion.value,
-      identificacionLider: formulario.identLider.value,
-      nombreLider: formulario.identVotante.value,
-    }),
-  });
+  registro.classList.remove("activo");
+  exito.classList.add("activo");
+
 });
+
+
+    
+ 
